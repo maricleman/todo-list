c84c47bae6c4803e3c0b92db4b91c67a983e0764
David Maricle <david.maricle@abbott.com>
2021-10-12 08:45:41 -0500

    giving thanks where thanks is due

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,14 @@ import Title from './form-components/Title';
 import AddItem from './form-components/AddItem';
 import TodoListTable from './table/TodoListTable';
 
+/**
+ * I want to extend a thanks to this example for
+ * ideas on how to style the application:
+ * https://nuflakbrr-todolist.vercel.app/
+ * https://github.com/nuflakbrr/react-todolist/blob/master/src/components/List.js
+ * 
+ * @returns The Todo Application in its entirety.
+ */
 function App() {
     const [itemsInTodoList, setItemsInTodoList] = useState(['']);
 
