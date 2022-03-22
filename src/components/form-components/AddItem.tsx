778b7e471e9199d00eeaccf9a0c97930bae347b4
David Maricle <david.maricle@abbott.com>
2022-03-22 10:54:07 -0500

    Validating that the user has inputted in text first, and, if not, showing a modal.

--- src/components/form-components/AddItem.tsx
+++ src/components/form-components/AddItem.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react'
 import ResourceManager from '../ResourceManager';
 import { useInput } from '../hooks/useInput';
 import styles from './AddItemStyles.scss';
+import Modal from 'react-modal';
 
 // const cx = classNames.bind();
 type AppProps = {
@@ -21,6 +22,9 @@ export const AddItem: React.FC<AppProps> = (props) => {
     const { value, setValue, reset } = useInput('');
     const [newItemInList, setNewItemInList] = useState("");
     const stringResources = useContext(ResourceManager);
+    const [openModal, setOpenModal] = useState(false);
+    const [modalSubtitle, setModalSubtitle] = useState('');
+    let subtitle;
 
     /**
      * Captures the item the user submitted
@@ -30,9 +34,59 @@ export const AddItem: React.FC<AppProps> = (props) => {
      */
     const handleAddItemToList = (evt) => {
         evt.preventDefault();
-        handleAddNewItemToList(value);
-        reset();
+        // Validate input
+        if (value === '') {
+            // Inform the user they need to insert data first
+            setOpenModal(true);
+        } else {
+            handleAddNewItemToList(value);
+            reset();
+        }
     }
+      
+      // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+      Modal.setAppElement('#root');
+
+    /**
+     * Function to close the modal
+     * @param evt 
+     */
+    const handleModalAfterClose = (evt) => {
+        setOpenModal(false);
+    }
+
+    const handleModalAfterOpen = (evt) => {
+        console.log('Modal is opened!');
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+        subtitle = "Empty input";
+        setModalSubtitle('Empty input');
+    }
+
+    const modalStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    const closeModalButtonStyles = {
+        backgroundColor: '#0078d0',
+        border: 0,
+        borderRadius: '56px',
+        color: '#fff',
+        cursor: 'pointer',
+        display: 'inline-block',
+        fontFamily: 'system-ui,-apple-system,system-ui,"Segoe UI",Roboto,Ubuntu,"Helvetica Neue",sans-serif',
+        fontSize: '18px',
+        fontWeight: 600,
+        outline: 0,
+        padding: '10px',
+     };
 
 
     return (
@@ -47,6 +101,20 @@ export const AddItem: React.FC<AppProps> = (props) => {
             >
             </input>
             <button type="submit" className={styles.submitButton}>Add</button>
+            <Modal
+                isOpen={openModal}
+                onAfterOpen={handleModalAfterOpen}
+                onRequestClose={handleModalAfterClose}
+                style={modalStyles}
+                contentLabel="Empty Content Modal"
+            >
+                <h2 style={{display: 'flex', justifyContent: 'center'}}>Empty input</h2>
+                <h4>Please enter a value before adding something to the list.</h4>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <button style={closeModalButtonStyles} onClick={handleModalAfterClose}>close</button>
+                </div>
+                
+            </Modal>
         </form>
     );
 }
