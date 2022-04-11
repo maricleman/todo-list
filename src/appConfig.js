8517679f7f8b03f4ac19075de064a63bfa78cec9
David Maricle <david.maricle@abbott.com>
2022-04-11 16:31:12 -0500

    Adding  https

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,6 +1,6 @@
 const appConfig = {
     // todoManagerApiUrl: 'https://maricle-todo-list-api.azurewebsites.net/api/TodoItems'
-    todoManagerApiUrl: 'maricle-todo-list-api.privatelink.azurewebsites.net/api/TodoItems'
+    todoManagerApiUrl: 'https://maricle-todo-list-api.privatelink.azurewebsites.net/api/TodoItems'
     // todoManagerApiUrl: 'https://localhost:44373/api/TodoItems'
 }
 
