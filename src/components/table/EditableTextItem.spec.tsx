3be1a30563a6dbfedd57bb3c0190ce214551635a
David Maricle <david.maricle@abbott.com>
2022-05-03 08:14:29 -0500

    Not running tests for now.

--- src/components/table/EditableTextItem.spec.tsx
+++ src/components/table/EditableTextItem.spec.tsx
@@ -1,10 +1,10 @@
-import '@testing-library/jest-dom';
-import { EditableTextItem } from './EditableTextItem';
-import TodoItem from '../common/TodoItem';
-import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
-import { useRef, useState } from 'react';
-import React from 'react';
+// import '@testing-library/jest-dom';
+// import { EditableTextItem } from './EditableTextItem';
+// import TodoItem from '../common/TodoItem';
+// import userEvent from '@testing-library/user-event';
+// import { render, screen } from '@testing-library/react';
+// import { useRef, useState } from 'react';
+// import React from 'react';
 
 // Look at this: https://github.com/escreengithub/arc/blob/double-manual-barcode/libs/arc-core/src/lib/input-text-field/InputTextField.spec.tsx
 
