10c31ed1899b919795c57e11577d024b5ed4a5e1
David Maricle <david.maricle@abbott.com>
2022-05-03 08:01:13 -0500

    Changing azure webapp name.

--- src/components/table/TableRowItem.spec.tsx
+++
@@ -1,5 +0,0 @@
-
-import '@testing-library/jest-dom';
-import { TableRowItem } from './TableRowItem';
-import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
