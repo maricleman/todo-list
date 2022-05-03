6a3be84cfc391940e761b3d46d6141f65227ccb6
David Maricle <david.maricle@abbott.com>
2022-05-03 16:22:09 -0500

    Saving the unit test that I started.

--- src/components/table/EditableTextItem.spec.tsx
+++ src/components/table/EditableTextItem.spec.tsx
@@ -1,41 +1,34 @@
-// import '@testing-library/jest-dom';
-// import { EditableTextItem } from './EditableTextItem';
-// import TodoItem from '../common/TodoItem';
-// import userEvent from '@testing-library/user-event';
-// import { render, screen } from '@testing-library/react';
-// import { useRef, useState } from 'react';
-// import React from 'react';
+import '@testing-library/jest-dom';
+import { EditableTextItem } from './EditableTextItem';
+import TodoItem from '../common/TodoItem';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { useRef, useState } from 'react';
+import React from 'react';
 
-// // Look at this: https://github.com/escreengithub/arc/blob/double-manual-barcode/libs/arc-core/src/lib/input-text-field/InputTextField.spec.tsx
+// Look at this: https://github.com/escreengithub/arc/blob/double-manual-barcode/libs/arc-core/src/lib/input-text-field/InputTextField.spec.tsx
 
-// test('renders successfully', () => {
+test('renders successfully', () => {
 
-//     // Arrange
-//     const myTodoItem = new TodoItem('Mowing the yard');
-//     const [value, setValue] = useState('');
-//     const userJustClosedModal = false;
-//     const saveButton = useRef<any>();
+    // Arrange
+    const myTodoItem = new TodoItem('Mowing the yard');
 
+    // Act
+    render(
+        <EditableTextItem
+            todoItem={myTodoItem}
+            isEditable={false}
+            value=''
+            setValue={() => { }}
+            userJustClosedModal={false}
+            handleSetUserJustClosedModal={() => { }}
+            handleDeletingItemInToDoList={(idToDelete: number) => { }}
+            handleToggleIsEditableFlag={() => { }}
+        />
+    );
 
-//     const handleSetUserJustClosedModal = () => {
+    // Assert
+    const saveBtn = screen.getByText('Save');
+    expect(saveBtn).toBeInTheDocument();
 
-//     };
-
-//     // Act
-//     render(
-//         <EditableTextItem
-//             todoItem={myTodoItem}
-//             isEditable={false}
-//             value={value}
-//             setValue={setValue}
-//             saveButton={saveButton.current}
-//             userJustClosedModal={userJustClosedModal}
-//             handleSetUserJustClosedModal={handleSetUserJustClosedModal}
-//         />
-//     );
-
-//     // Assert
-//     const input = screen.getByText(value);
-//     expect(input).toBeInTheDocument();
-
-// });
+});
